b2c4a47021c33cc369664ac034d54b68093b2e02
Joren Van Onder <joren@jvo.sh>
2021-09-12 08:22:42 -0700

    Add license to python

--- crash_recorder/__manifest__.py
+++ crash_recorder/__manifest__.py
@@ -1,6 +1,20 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
+# coding: utf-8
+# Copyright (c) 2021, Joren Van Onder <joren@jvo.sh>
+# 
+# This file is part of crash-recorder.
+# 
+# crash-recorder is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# crash-recorder is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with crash-recorder.  If not, see <https://www.gnu.org/licenses/>.
 {
     "name": "Crash Recorder",
     "category": "Tools",
@@ -15,4 +29,5 @@ Automatically record user actions leading to errors.
         "security/ir.model.access.csv",
     ],
     "qweb": ["static/src/xml/rrweb_field.xml"],
+    "license": "LGPL-3",
 }
