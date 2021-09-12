ac427208c6560283c063cd8b11e4d788a599d0c4
Joren Van Onder <joren@jvo.sh>
2021-09-12 08:23:37 -0700

    Add copyright to JS

--- crash_recorder/static/src/js/rrweb.js
+++ crash_recorder/static/src/js/rrweb.js
@@ -1,3 +1,19 @@
+// Copyright (c) 2021, Joren Van Onder <joren@jvo.sh>
+//
+// This file is part of crash-recorder.
+//
+// crash-recorder is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// crash-recorder is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with crash-recorder.  If not, see <https://www.gnu.org/licenses/>.
 odoo.define('crash_recorder.rrweb', function (require) {
     "use strict";
 
