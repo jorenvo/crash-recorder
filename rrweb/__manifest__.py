7cbc66d18730ce8b5e014ed0ed6e8b3632ac91cb
Joren Van Onder <joren@jvo.sh>
2021-09-11 19:17:44 -0700

    Black

--- rrweb/__manifest__.py
+++ rrweb/__manifest__.py
@@ -2,19 +2,17 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Record errors',
-    'category': 'Hidden', # todo
-    'description': """
+    "name": "Record errors",
+    "category": "Hidden",  # todo
+    "description": """
 Automatically record user actions leading to errors.
 """,
-    'version': '1.0',
-    'depends': ['web'],
-    'data': [
-        'views/rrweb_views.xml',
-        'views/rrweb_templates.xml',
-        'security/ir.model.access.csv',
+    "version": "1.0",
+    "depends": ["web"],
+    "data": [
+        "views/rrweb_views.xml",
+        "views/rrweb_templates.xml",
+        "security/ir.model.access.csv",
     ],
-    'qweb': [
-        'static/src/xml/rrweb_field.xml',
-    ],
+    "qweb": ["static/src/xml/rrweb_field.xml",],
 }
--- rrweb/models/rrweb.py
+++ rrweb/models/rrweb.py
@@ -3,19 +3,26 @@ from odoo import api, fields, models, SUPERUSER_ID, _
 from base64 import b64encode, b64decode
 import zlib
 
+
 class RRWebRecording(models.Model):
     _name = "rrweb.recording"
     _description = "Recordings of user actions leading up to a crash"
     _rec_name = "create_date"
     _order = "create_date desc"
 
-    events = fields.Binary("Events", compute="_compute_events", inverse="_inverse_events", store=False)
-    events_compressed = fields.Binary("Compressed Events", help="Raw rrweb JSON events", attachment=True)
+    events = fields.Binary(
+        "Events", compute="_compute_events", inverse="_inverse_events", store=False
+    )
+    events_compressed = fields.Binary(
+        "Compressed Events", help="Raw rrweb JSON events", attachment=True
+    )
     error = fields.Text("Error", help="Error that occurred in this recording")
 
     def _compute_events(self):
         for recording in self:
-            recording.events = b64encode(zlib.decompress(b64decode(recording.events_compressed)))
+            recording.events = b64encode(
+                zlib.decompress(b64decode(recording.events_compressed))
+            )
 
     def _inverse_events(self):
         """ A recording can be >3 MiB, so compress it. """
