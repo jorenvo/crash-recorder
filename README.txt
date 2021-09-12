c37b01755d0b14b3e7416e02e678a85dace5fd0c
Joren Van Onder <joren@jvo.sh>
2021-09-12 07:52:35 -0700

    Rename to crash_recorder

--- README.txt
+++ README.txt
@@ -2,3 +2,4 @@ TODO
 ====
 - cron to delete old recordings
 - rename to Crash Recorder (crash_recorder)
+- license
--- crash_recorder/__manifest__.py
+++ crash_recorder/__manifest__.py
@@ -2,16 +2,16 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    "name": "Record errors",
-    "category": "Hidden",  # todo
+    "name": "Crash Recorder",
+    "category": "Tools",
     "description": """
 Automatically record user actions leading to errors.
 """,
     "version": "1.0",
     "depends": ["web"],
     "data": [
-        "views/rrweb_views.xml",
-        "views/rrweb_templates.xml",
+        "views/rrweb_recording_views.xml",
+        "views/crash_recorder_templates.xml",
         "security/ir.model.access.csv",
     ],
     "qweb": ["static/src/xml/rrweb_field.xml",],
